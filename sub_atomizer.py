ff5e0209de8c3d3b94ab925dced1ba3a2f7f54ff
unknown <michael.j.lukiman@gmail.com>
2019-03-24 23:38:35 -0400

    implements positivity

---
+++ sub_atomizer.py
@@ -0,0 +1,103 @@
+from typing import Dict, List
+from enum import Enum
+import sub_parser as sp
+import sys
+import textwrap
+
+class Atom(Enum):
+    VAL=0
+    ASS=1
+
+def clausify_single(atoms: List, booleanAss, reason):
+    '''
+    Takes a list of single atoms and establishes them all to a given truth value.
+    They are singletons which we are batch-defining for ease of use.
+    '''
+    clausified = [] 
+    for atom in atoms:
+        fullAtom = [[booleanAss]+atom+['val']]
+        clausified.append([fullAtom, reason])
+    return clausified
+
+def necessitate_truth(atoms: List, reason):
+    '''
+    All atoms in this list will always yearn to be true.
+    '''
+    return clausify_single(atoms, ' ', reason)
+
+def necessitate_falsehood(atoms: List, reason):
+    '''
+    All atoms in this list will always yearn to be false.
+    '''
+    return clausify_single(atoms, 'NOT', reason)
+
+def makeFullAtom(boolVal: bool, slot1, slot2, time: int, type: int):
+    '''
+    Makes creating atoms slightly faster.
+    '''
+    notStat = ' ' if bool else 'NOT'
+    typeStr = 'val' if Atom.type.value==1 else 'ass'
+    resList = [notStat, slot1, slot2, time, typeStr]
+
+def atomized_uniqueness(value_atoms: List, values: List, time: int):
+    '''
+    Takes a set of established values and returns the values that cannot coexist with these.
+    '''
+    unique_val_nots = []
+    for i, atom in enumerate(value_atoms):
+        for i, val in enumerate(values):
+            if atom[1]!=val:
+                unique_val_nots.append([atom[0],val,time])
+    return unique_val_nots
+
+def atomized_state(state: Dict, time: int):
+    '''
+    Takes a state and returns the next values in a tuple with the values that cannot coexist.
+    '''
+    value_atoms = []
+    values = []
+    for i, key in enumerate(state):
+        value_atoms.append([key, state[key], time])
+        values.append(state[key])
+    unique_val_nots = atomized_uniqueness(value_atoms, values, time)
+    return value_atoms, unique_val_nots
+    
+def clausified_positivity(ass: List, initState: Dict):
+    '''
+    (Ass(RA,RB,I)^Val(RB,V,I)) -> Val(RA,V,I+1) 
+    == XAss v XVal v Val [via DeMorgans and implication removal]
+    '''
+    afterstate = []
+    writeToRA = asstuple[0]
+    writeFromRB = asstuple[1]
+    atTimeI = asstuple[2]
+    rbv = initState[writeFromRB]
+    XAss = makeFullAtom(False, writeToRA, writeFromRB, atTimeI, ASS)
+    XVal = makeFullAtom(False, writeFromRB, rbv, atTimeI, VAL)
+    Val = makeFullAtom(True, writeToRA, rbv, atTimeI+1, VAL)
+    return [[XAss, XVal, Val], 'positivity']
+
+
+def populate(reqs: Dict):
+    start, antistart = atomized_state(reqs['START'], 0)
+    goal, antigoal = atomized_state(reqs['GOAL'], reqs['LIMIT'])
+    clausified = necessitate_truth(start+goal, 'want true') + necessitate_falsehood(antistart+antigoal, 'want false') 
+    return clausified 
+
+def printClauses(clauses: List, preferredWidth = 70):
+    for i, clause in enumerate(clauses):
+        prefix = "Clause "+str(i+1)+"\t"
+        wrapper = textwrap.TextWrapper(initial_indent=prefix, width=preferredWidth,
+                            subsequent_indent=' '*len(prefix))
+        message = wrapper.fill(str(clause[1])+"\t"+str(clause[0]))
+        print(message)
+
+if __name__ == "__main__":
+    try:
+        path = sys.argv[1]
+    except:
+        print("Please input the path of the register requirements.") 
+    else:
+        reqs = sp.read_in(path)
+        clauses = populate(reqs)
+        printClauses(clauses)
--- sub_parser.py
+++ sub_parser.py
@@ -1,6 +1,52 @@
+from enum import Enum
+from typing import List
+import sys
+import re
+
+class Row(Enum):
+    START = 0
+    GOAL = 1
+    LIMIT = 2
+    
+def populatePairs(chars: List[str]):
+    res = {}
+    for i, char in enumerate(chars):
+        if char.isalnum() and i % 2 == 0:
+            try:
+                res[int(char)] = chars[i+1]
+            except:
+                print("There seems to be an unpaired item.")
+    return res
 
 def read_in(filePath: str):
+    start = {}
+    goal = {}
+    limit = 0
+    row = -1
+    with open(filePath, "r") as file:
+        for line in file:
+            chars = re.split(" |\t|\r|\n", line) 
+            if chars[0].isalnum():
+                row += 1
+            if row==Row.START.value:
+                start = populatePairs(chars)
+            elif row==Row.GOAL.value:
+                goal = populatePairs(chars)
+            elif row==Row.LIMIT.value:
+                for char in chars:
+                    if char.isdigit():
+                        limit = int(char) 
+                        break
+                else: 
+                    print("Could not find step limit.")
+    requirements = {}
+    requirements['START'], requirements['GOAL'], requirements['LIMIT'] = start, goal, limit
+    return requirements
+
+if __name__ == "__main__":
     try:
-        with file as open(filePath, "r"):
-            for line in file:
-                if 
+        path = sys.argv[1]
+    except:
+        print("Please input the path of the register requirements.") 
+    else:
+        print(read_in(path))
